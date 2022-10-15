9c677f9c3a4c12e857dbb86b7f0470f1ca6127c2
fe-lam-dinh <dinhtronglam172000@gmail.com>
2022-10-15 20:37:42 +0700

    #11 - Lamzp build the default-layout-frame

--- src/components/layout/DefaultLayout/index.js
+++ src/components/layout/DefaultLayout/index.js
@@ -1,14 +1,18 @@
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
+
 import Header from '~/components/layout/components/Header';
 import Sidebar from '~/components/layout/components/Sidebar';
 
+const cx = classNames.bind(styles);
 const DefaultLayout = ({ children }) => {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <main className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
-            </div>
+                <div className={cx('content')}>{children}</div>
+            </main>
         </div>
     );
 };
--- src/components/layout/components/Header/index.js
+++ src/components/layout/components/Header/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
 
+const cx = classNames.bind(styles);
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header className={cx('primary-header')}>
+            <div className={cx('container')}></div>
+        </header>
+    );
 };
 
 export default Header;
