e5749096ce80bdd0368147348e053e74f92b3320
fe-lam-dinh <dinhtronglam172000@gmail.com>
2022-10-22 19:31:58 +0700

    #15 - Lamzp building UI for header part 2

--- src/components/layout/components/Header/index.js
+++ src/components/layout/components/Header/index.js
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 import { faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-
+import Tippy from '@tippyjs/react/headless';
+import { Popper as PopperWrapper } from '~/components/Popper';
 import images from '~/assets/images';
+import AccountItem from '~/components/AccountItem';
 
 const cx = classNames.bind(styles);
 const Header = () => {
+    const [searchResult, setSearchResult] = useState(['w']);
     return (
         <header className={cx('primary-header')}>
             <div className={cx('container')}>
                 <div className={cx('logo')}>
                     <img src={images.logo} alt="logo Tiktok" />
                 </div>
-                <div className={cx('search')}>
-                    <input placeholder="Search account and video...." spellCheck={false} />
-                    <button className={cx('clear-icon')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    <button className={cx('loading-icon')}>
-                        <FontAwesomeIcon icon={faSpinner} />
-                    </button>
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
-                </div>
+                <Tippy
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Account</h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search account and video...." spellCheck={false} />
+                        <button className={cx('clear-icon')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <button className={cx('loading-icon')}>
+                            <FontAwesomeIcon icon={faSpinner} />
+                        </button>
+                        <Tippy content="search">
+                            <button className={cx('search-btn')}>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            </button>
+                        </Tippy>
+                    </div>
+                </Tippy>
                 <div className={cx('action')}>action</div>
             </div>
         </header>
