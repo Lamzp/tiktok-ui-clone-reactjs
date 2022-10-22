3fd2af5badaf043de68e4f672a8930d85386b471
fe-lam-dinh <dinhtronglam172000@gmail.com>
2022-10-22 20:47:23 +0700

    #15 - Lamzp UI for header part 2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img src="https://media.istockphoto.com/photos/woman-lifestyle-picture-id1317200799" alt="avatar" />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>DinhTrongLam</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCircleCheck} />
+                </p>
+                <span className={cx('user-name')}>lam@1234</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
